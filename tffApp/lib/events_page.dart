bb39a15e438efad0a04f9021075c93a0a2eaeffd
shantanudev679 <shantanudev679@gmail.com>
2021-03-07 15:57:00 +0530

    Events page added

---
+++ tffApp/lib/events_page.dart
@@ -0,0 +1,214 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class EventsPage extends StatelessWidget {
+  static const String id = 'events_page_id';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              children: [
+                Image.asset(
+                  'images/strawberry.jpg',
+                  height: 350,
+                  width: MediaQuery.of(context).size.width,
+                  fit: BoxFit.cover,
+                ),
+                Container(
+                  height: 350,
+                  color: Colors.black12,
+                  padding: EdgeInsets.only(top: 50),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.only(
+                          left: 24,
+                          right: 24,
+                        ),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.arrow_back,
+                              color: Colors.white,
+                              size: 30,
+                            ),
+                            Spacer(),
+                            Icon(
+                              Icons.share,
+                              color: Colors.white,
+                              size: 27,
+                            ),
+                            SizedBox(
+                              width: 16,
+                            ),
+                            Icon(
+                              Icons.thumb_up_alt,
+                              color: Colors.white,
+                              size: 27,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Spacer(),
+                      Container(
+                        padding: EdgeInsets.only(
+                          left: 24,
+                          right: 24,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Events Name",
+                              style: GoogleFonts.lato(
+                                textStyle: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 23,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 12,
+                            ),
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.location_on,
+                                  color: Colors.white70,
+                                  size: 25,
+                                ),
+                                SizedBox(
+                                  width: 8,
+                                ),
+                                Text(
+                                  "Venue Of the Event",
+                                  style: GoogleFonts.lato(
+                                    textStyle: TextStyle(
+                                      color: Colors.white70,
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 8,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 18,
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(30),
+                            topLeft: Radius.circular(30.0),
+                          ),
+                        ),
+                        height: 50,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              height: 405,
+              child: Padding(
+                padding: EdgeInsets.only(left: 24.0, right: 24.0, top: 24.0,),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Description :',
+                        style: GoogleFonts.lato(
+                          textStyle: TextStyle(
+                            fontSize: 23,
+                            fontWeight: FontWeight.w600,
+                            letterSpacing: 1.7,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text('Here comes the description of the event. I was not able to find anything on instagram about an event or its description so i am just randomly brabring something in this column. Since you have got so far reading this that means you are so idle and just stop reading now kyunki iske aage tumhe kuch kaam ka nahi milne wala so lamo.',
+                        style: GoogleFonts.lato(
+                          textStyle: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w400,
+                            letterSpacing: 1.2,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 18.0,
+                      ),
+                      Text('Images',
+                        style: GoogleFonts.lato(
+                          textStyle: TextStyle(
+                            fontSize: 23,
+                            fontWeight: FontWeight.w600,
+                            letterSpacing: 1.7,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 18.0,
+                      ),
+                      Row(
+                        children: [
+
+                        ],
+                      ),
+                      SizedBox(
+                        height: 30.0,
+                      ),
+                      Center(
+                        child: GestureDetector(
+                          onTap:(){},
+                          child: Container(
+                            height: 50.0,
+                            width: MediaQuery.of(context).size.width/1.5,
+                            decoration: BoxDecoration(
+                              color: Colors.lightBlue,
+                              borderRadius: BorderRadius.all(Radius.circular(24.0)),
+                            ),
+                            child: Center(
+                              child: Text(
+                                'Register',
+                                style: GoogleFonts.lato(
+                                  textStyle: TextStyle(
+                                    fontSize: 19.0,
+                                    color: Colors.white70,
+                                    fontWeight: FontWeight.w600
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
